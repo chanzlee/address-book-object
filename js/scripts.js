9b6e58a6533407dddde6623a91056b9201227c0c
Chan Lee and Hyewon Cho <hyewon@>
2018-08-21 09:51:28 -0700

    Clear inputboxes when user submits form

--- js/scripts.js
+++ js/scripts.js
@@ -22,33 +22,30 @@ Address.prototype.fullAddress = function() {
 
 $(document).ready(function(){
   // var addressForm = $("#new-addresses").first().html();
-  var addressFormTop = '<div class="new-address"><h4>Address Type</h4>';
-  var addressFormMiddle = '<div class="radio"><label><input type="radio" name="addressType0" value="home" checked>Home</label></div><div class="radio"><label><input type="radio" name="addressType0" value="office">Office</label></div>';
-  var addressFormBottom = '<div class="form-group"><label for="street">Street</label><input type="text" class="street form-control"></div><div class="form-group"><label for="city">City</label><input type="text" class="city form-control"></div><div class="form-group"><label for="state">State</label><input type="text" class="state form-control"></div></div>'
+  var addressForm = '<div class="new-address"><h4>Address Type</h4><div class="radio"><label><input type="radio" name="addressType0" value="home" checked>Home</label></div><div class="radio"><label><input type="radio" name="addressType0" value="office">Office</label></div><div class="form-group"><label for="street">Street</label><input type="text" class="street form-control"></div><div class="form-group"><label for="city">City</label><input type="text" class="city form-control"></div><div class="form-group"><label for="state">State</label><input type="text" class="state form-control"></div></div>'
 
   var contacts = [];
   var id = 0;
-  var addressFormCount = 0;
+  var addressCreateCount = 0;
+  var initialForm = $("#new-contact").html();
 
 
   $("#addForm").click(function(event){
     event.preventDefault();
-    addressFormCount++;
-    var addressFormMiddleNew = addressFormMiddle.replace(/name="addressType0"/g, 'name="addressType'+addressFormCount+'"');
-
-    var addressForm = addressFormTop+addressFormMiddleNew+addressFormBottom;
-    $("#new-addresses").append(addressForm);
+    addressCreateCount++;
+    var addressFormNew = addressForm.replace(/name="addressType0"/g, 'name="addressType'+addressCreateCount+'"');
+    $("#new-addresses").append(addressFormNew);
   });
   $("#new-contact").submit(function(event){
     event.preventDefault();
     var contact = new Contact(id++, $("#firstName").val(), $("#lastName").val());
-    var index=0;
+    var addressReadCount=0;
     $(".new-address").each(function(){
-      contact.addresses.push(new Address($("input:radio[name=addressType"+index+"]:checked").val(), $(".street").val(), $(".city").val(), $(".state").val()));
-      index++;
+      contact.addresses.push(new Address($("input:radio[name=addressType"+addressReadCount+"]:checked").val(), $(".street").val(), $(".city").val(), $(".state").val()));
+      addressReadCount++;
     });
     contacts.push(contact);
-    $("#contactsDisplay").append("<li>"+contact.name()+"<span class='hidden id'>"+contact.id+"</span>"+"</li>")
+    $("#contactsDisplay").append("<li>"+contact.name()+"<span class='hidden id'>"+contact.id+"</span>"+"</li>");
     $("#contactsDisplay").children().last().click(function(){
       var id = parseInt($(this).find(".id").text());
       var contact = contacts.find(function(contact) {
@@ -56,10 +53,12 @@ $(document).ready(function(){
       });
       $(".firstNameDisplay").text(contact.firstName);
       $(".lastNameDisplay").text(contact.lastName);
+      $("#addressesDisplay").text("");
       contact.addresses.forEach(function(address){
         $("#addressesDisplay").append("<li>"+address.fullAddress()+"</li>");
       });
     });
-    addressFormCount = 0;
+    addressCreateCount = 0;
+    $("#new-contact").html(initialForm);
   });
 });
